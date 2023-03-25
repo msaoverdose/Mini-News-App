2151c48ef47fe5ba03adbbeab58f61e564d1de92
Mehmet Semih Arslan <49487581+dmsa2003@users.noreply.github.com>
2023-03-25 09:54:33 +0100

    Update news.py

--- news.py
+++ news.py
@@ -6,7 +6,7 @@ class NewsInterface:
     def __init__(self, master):
         self.master = master
         master.title("News App By DMSA") #You can change this title to your own title
-        self.news_listbox = tk.Listbox(master, width=60)
+        self.news_listbox = tk.Listbox(master, width=60) #You can use different resolutions for better interface 
         self.news_listbox.grid(row=0, column=0, padx=10, pady=10)
         self.news_listbox.bind("<Double-Button-1>", self.open_article) 
         self.feed = feedparser.parse("http://feeds.bbci.co.uk/news/world/rss.xml#") #We've used bbci for source but you can take advantage to different sources
